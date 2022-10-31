38d7b256bc1b4838036c490cb812cf7b923eab63
Expert <Expert@DESKTOP-COITNQS>
2022-10-30 21:42:37 -0800

    project maven

---
+++ src/basic/test1.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class test1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hi");
+		
+		m1();
+		
+		
+	}
+    public static  void m1()
+    {
+    	System.out.println("Good morning");
+    }
+	
+}
